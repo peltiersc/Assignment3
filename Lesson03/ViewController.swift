10de0904baed61d3c21fdb700471b5673c473d70
peltiersc <punkyhoya@gmail.com>
2015-05-05 13:26:23 -0400

    Remaining Todos (2 & 3) completed

--- Lesson03/ViewController.swift
+++ Lesson03/ViewController.swift
@@ -11,33 +11,33 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBAction func modalViewLoad(recognizer:UISwipeGestureRecognizer) {
-        
-        var modalView = UIViewController()
-        
-        self.presentViewController(modalView, animated: true, completion: nil)
-        
-        modalView.view.backgroundColor = UIColor.redColor()
-        
+        let newView = storyboard?.instantiateViewControllerWithIdentifier("modalView") as! UIViewController
+        self.presentViewController(newView, animated: true, completion: nil)
     }
     
+    @IBOutlet weak var dismissBtn: UIButton!
+    
+    @IBAction func dismissNewView() {
+       self.dismissViewControllerAnimated(true, completion: nil)
+    }
     
     @IBOutlet weak var displayTableViewBtn: UIButton!
     
     @IBAction func displayTableView(sender: UIButton) {
-        
         self.performSegueWithIdentifier(
             "showTableView", sender: displayTableViewBtn)
-    }
     
-    /* Stuart - would this be better?
+    /* Stuart - would this be better? does it matter?
     
     self.performSegueWithIdentifier(
     "showTableView", sender: self)
     
     ?
     */
+        
+    }
+    
 }
-
 
 /*
 TODO one: Hook up a swipeable area on the home screen that must present a modal dialog when swiped. You must create the modal dialog and present it in CODE (not the storyboard).
@@ -48,8 +48,6 @@ TODO three: Add and hook up a ‘dismiss’ button below the above mentioned ima
 
 TODO four: Hook up the button on the home screen to push ArrayTableViewController into view (via the navigation controller) when tapped. Do this by triggering a segue from this view controller. The method you are looking for is performSegueWithIdentifier. Find the identifier from the storyboard.
 */
-
-
 
 
 
